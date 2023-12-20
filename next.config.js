dc1cd32bd6b44d69fd2ba8d989aae6a05556654a
azkajulda <azkajulda@gmail.com>
2023-12-20 14:47:17 +0700

    fix config next

--- next.config.js
+++ next.config.js
@@ -1,15 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  exportPathMap: async function () {
-    return {
-      '/': {page: '/pages/login'},
-      '/login': {page: '/pages/login'},
-      '/register': {page: '/pages/register'},
-      '/home': {page: '/pages/home'},
-      '/browse': {page: '/pages/browse'},
-      '/testimonial': {page: '/pages/testimonial'},
-    }
-  },
-}
+const nextConfig = {}
 
 module.exports = nextConfig
